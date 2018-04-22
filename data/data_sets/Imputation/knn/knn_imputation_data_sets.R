1d0078d6ca0e1f3d500b4262237e2d94aae39d26
SamirAdrik <saad@nmbu.no>
2018-04-22 13:25:59 +0200

    Missing values distribution files

--- data/data_sets/Imputation/knn/knn_imputation_data_sets.R
+++ data/data_sets/Imputation/knn/knn_imputation_data_sets.R
@@ -1,67 +1,107 @@
-# Install relevant packages
-# source("https://bioconductor.org/biocLite.R")
-# biocLite("impute")
-# install.packages("reporttools")
-# install.packages("VIM")
-
+# ----------------------------------------------------------- #
+# Install relevant packages (if not already done)
+# ----------------------------------------------------------- #
+bio_conductor <- "https://bioconductor.org/biocLite.R"
+source(bio_conductor)
+Packages <- c("reporttools", "VIM", "tikzDevice", "Hmisc",
+              "impute")
+# install.packages(Packages)
+# ----------------------------------------------------------- #
 # Load relevant packages and source helper functions
-library(Hmisc)
-library(impute)
-library(reporttools)
-library(VIM)
-library(tikzDevice)
-
+# ----------------------------------------------------------- #
+lapply(Packages, library, character.only = T)
 source("../_helper_func.R")
 
+# ----------------------------------------------------------- #
 # Load HFpEF and HFmrEF datafiles
+# ----------------------------------------------------------- #
 load("../../raw_data/data_use_HFpEF_matrix.Rdat") 
 load("../../raw_data/data_use_HFmrEF_matrix.Rdat") 
 
+# ----------------------------------------------------------- #
 # Replace NaN values with NA using the make_na function
+# ----------------------------------------------------------- #
 HFpEF_matrix <- make_na(HFpEF_matrix)
 HFmrEF_matrix <- make_na(HFmrEF_matrix)
 
-# Remove the indicator variables as first step of analysis
+# ----------------------------------------------------------- #
+# Store indicator and non-indicator variables in seperate
+# variables using the rm_indicator function
+# ----------------------------------------------------------- #
 HFpEF_rm_ind <- rm_indicator(HFpEF_matrix, n_uniq = 5)
 HFmrEF_rm_ind <- rm_indicator(HFmrEF_matrix, n_uniq = 5)
 
+# ----------------------------------------------------------- #
+# Store the non-indicator variables for later
+# ----------------------------------------------------------- #
 HFpEF_matrix_not_ind <- HFpEF_rm_ind$non_indicator
 HFmrEF_matrix_not_ind <- HFmrEF_rm_ind$non_indicator
 
+# ----------------------------------------------------------- #
 # Store the indicator variables for later
+# ----------------------------------------------------------- #
 HFpEF_matrix_ind_var <- HFpEF_rm_ind$indicator
 HFmrEF_matrix_ind_var <- HFmrEF_rm_ind$indicator
 
+# ----------------------------------------------------------- #
 # Move some variables between matrices in HFmrEF dataset
+# ----------------------------------------------------------- #
 # Change RVfunction from non-indicator to indicator variable
-HFmrEF_RV <- move_columns(HFmrEF_matrix_not_ind, HFmrEF_matrix_ind_var, 
+# ----------------------------------------------------------- #
+HFmrEF_RV <- move_columns(HFmrEF_matrix_not_ind, 
+                          HFmrEF_matrix_ind_var, 
                           "rvfunction")
 HFmrEF_matrix_ind_var<- HFmrEF_RV$to_mat
 HFmrEF_matrix_not_ind <- HFmrEF_RV$from_mat
 
-# Change BmIadmission from indicator variable to non_indicator variables
-HFmrEF_BMI <- move_columns(HFmrEF_matrix_ind_var, HFmrEF_matrix_not_ind, 
+# ----------------------------------------------------------- #
+# Change BmIadmission from indicator variable to non_indicator 
+# variable
+# ----------------------------------------------------------- #
+HFmrEF_BMI <- move_columns(HFmrEF_matrix_ind_var, 
+                           HFmrEF_matrix_not_ind, 
                            "bmiadmission")
 HFmrEF_matrix_ind_var<- HFmrEF_BMI$from_mat
 HFmrEF_matrix_not_ind <- HFmrEF_BMI$to_mat
 
-# Convert zeros to missings, the following variables are not to be converted.
-not_zeros_HFpEF <- c("comorbidities", "weightchange", "daysfollowupdischarge", 
-                     "timetonextadm")
-not_zeros_HFmrEF <- c("numbercomorditiesnoida", "comorbidities", "timetoadmission",
-                      "timetoecho", "timetofollowupfrombnp", 
+# ----------------------------------------------------------- #
+# Convert zeros to missings, the following variables are not to 
+# be converted.
+# ----------------------------------------------------------- #
+not_zeros_HFpEF <- c("comorbidities", "weightchange", 
+                     "daysfollowupdischarge", "timetonextadm")
+not_zeros_HFmrEF <- c("numbercomorditiesnoida","comorbidities", 
+                      "timetoadmission", "timetoecho", 
+                      "timetofollowupfrombnp", 
                       "timetofollowupfromdischarge",
                       "timetofirstcardiachospitalisation")
-HFpEF_matrix_not_ind <- zero_to_na(HFpEF_matrix_not_ind, not_zeros_HFpEF)
-HFmrEF_matrix_not_ind <- zero_to_na(HFmrEF_matrix_not_ind, not_zeros_HFmrEF)
+HFpEF_matrix_not_ind <- zero_to_na(HFpEF_matrix_not_ind, 
+                                   not_zeros_HFpEF)
+HFmrEF_matrix_not_ind <- zero_to_na(HFmrEF_matrix_not_ind, 
+                                    not_zeros_HFmrEF)
 
+# ----------------------------------------------------------- #
 # Plot of missing valus distribution
-aggr(cbind(HFpEF_matrix_not_ind, HFpEF_matrix_ind_var), plot = T, 
-     sortVars = T, bars = F, combined = T, ylabs = "", cex.axis = 0.7)
-aggr(cbind(HFmrEF_matrix_not_ind, HFmrEF_matrix_ind_var), plot = T, 
-     sortVars = T, bars = F, combined = T, ylabs = "", cex.axis = 0.7)
-  
+# ----------------------------------------------------------- #
+path_to_images <- "../../../../doc/thesis/images/"
+
+pdf(file=paste(c(path_to_images,"HFpEF_miss_dist.pdf"), 
+               collapse = ""))
+aggr(cbind(HFpEF_matrix_not_ind, HFpEF_matrix_ind_var), 
+     plot = T, sortVars = T, bars = F, combined = T, 
+     ylabs = "", cex.axis = 0.7)
+dev.off()
+
+pdf(file = paste(c(path_to_images, "HFmr_miss_dist.pdf"),
+                 collapse = ""))
+aggr(cbind(HFmrEF_matrix_not_ind, HFmrEF_matrix_ind_var), 
+     plot = T, sortVars = T, bars = F, combined = T, 
+     ylabs = "", cex.axis = 0.7)
+dev.off()
+
+# ----------------------------------------------------------- #
 # Summary of variables
+# ----------------------------------------------------------- #
 cap_desc_HFpEF <- "Patient characteristics: HFpEF variables"
 lab_desc_HFpEF <- "tab:desc_stat_HFpEF_variables"
 tableContinuous(as.data.frame(HFpEF_matrix), 
