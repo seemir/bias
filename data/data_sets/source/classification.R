7b8edd9b99cd3268b16188aa118618d94303cd7a
SamirAdrik <saad@nmbu.no>
2018-09-23 17:14:51 +0200

    Added version of classification code

--- data/data_sets/source/classification.R
+++ data/data_sets/source/classification.R
@@ -1,7 +1,7 @@
 # ----------------------------------------------------------- #
 # Install relevant packages (if not already done)
 # ----------------------------------------------------------- #
-Packages <- c("mlbench", "caret")
+Packages <- c("mlbench", "caret", "elasticnet")
 # install.packages(Packages)
 
 # ----------------------------------------------------------- #
@@ -20,7 +20,8 @@ lapply(gsub(" ", "", paste("data_files/", allDataFiles,
 # ----------------------------------------------------------- #
 # Add cross validation configuration
 # ----------------------------------------------------------- #
-kfold <- trainControl(method = "cv", number = 10)
+kfold <- trainControl(method = "cv", number = 10, 
+                      returnResamp = "all")
 loocv <- trainControl(method = "LOOCV")
 seed <- 90210; metric <- "Accuracy"
 
@@ -32,18 +33,97 @@ mortality <- HFfullOutcomes[,3]
 readmission <- HFfullOutcomes[,4]
 
 # ----------------------------------------------------------- #
+# Mortality
+# ----------------------------------------------------------- #
 # kfold CV evaluation of classifiers
 # ----------------------------------------------------------- #
-fit.knn.kfold <- train(dataset, mortality, method="knn", 
-                       metric=metric, preProc="pca", 
-                       trControl=kfold)
+fitKnnKfoldMort <- train(dataset, mortality, method="knn", 
+                         metric=metric, trControl=kfold)
 
-fit.svm.kfold <- train(dataset, mortality, method="svmRadial", 
-                       metric=metric, preProc="pca", 
-                       trControl=kfold, fit = F)
+fitSvmKfoldMort <- train(dataset, mortality,method="svmRadial", 
+                         metric=metric, trControl=kfold)
 
-fit.rf.kfold <- train(dataset, mortality, method="rf", 
-                      metric = metric, preProcess = "pca", 
-                      trControl = kfold)
+fitRfKfoldMort <- train(dataset, mortality, method="rf", 
+                        metric = metric, trControl = kfold)
+
+# ----------------------------------------------------------- #
+# loocv CV evaluation of classifiers
+# ----------------------------------------------------------- #
+fitKnnLoocvMort <- train(dataset, mortality, method="knn", 
+                         metric=metric, trControl=loocv)
+
+fitSvmLoocvMort <- train(dataset, mortality,method="svmRadial", 
+                         metric=metric, trControl=loocv)
+
+fitRfLoocvMort <- train(dataset, mortality, method="rf", 
+                        metric = metric, trControl = loocv)
+
+# ----------------------------------------------------------- #
+# Produce summary statistics and plots
+# ----------------------------------------------------------- #
+# Kfold CV
+# ----------------------------------------------------------- #
+resultsMortalityKfold <- resamples(list(knn = fitKnnKfoldMort, 
+                                        svm = fitSvmKfoldMort,
+                                        rf = fitRfKfoldMort))
+
+summary(resultsMortalityKfold); dotplot(resultsMortalityKfold)
+
+# ----------------------------------------------------------- #
+# Loocv CV
+# ----------------------------------------------------------- #
+resultsMortalityLoocv <- resamples(list(knn = fitKnnLoocvMort,
+                                        svm = fitSvmLoocvMort,
+                                        rf = fitRfLoocvMort))
+
+summary(resultsMortalityLoocv); dotplot(resultsMortalityLoocv)
+
+# ----------------------------------------------------------- #
+# Readmission
+# ----------------------------------------------------------- #
+# kfold CV evaluation of classifiers
+# ----------------------------------------------------------- #
+fitKnnKfoldReadm <- train(dataset, readmission, method="knn", 
+                          metric=metric, trControl=kfold)
+
+fitSvmKfoldReadm <- train(dataset, readmission, 
+                          method="svmRadial", metric=metric,
+                          trControl=kfold)
+
+fitRfKfoldReadm <- train(dataset, readmission, method="rf", 
+                         metric = metric, trControl = kfold)
+
+# ----------------------------------------------------------- #
+# Loocv evaluation of classifiers
+# ----------------------------------------------------------- #
+fitKnnLoocvReadm <- train(dataset, readmission, method="knn", 
+                          metric=metric, trControl=loocv)
+
+fitSvmLoocvReadm <- train(dataset, readmission, 
+                          method="svmRadial", metric=metric,
+                          trControl=loocv)
+
+fitRfLoocvReadm <- train(dataset, readmission, method="rf", 
+                         metric = metric, trControl = loocv)
+
+# ----------------------------------------------------------- #
+# Produce summary statistics and plots
+# ----------------------------------------------------------- #
+# Kfold CV
+# ----------------------------------------------------------- #
+resultsReadmKfold <- resamples(list(knn = fitKnnKfoldReadm, 
+                                   svm = fitSvmKfoldReadm,
+                                   rf = fitRfKfoldReadm))
+
+summary(resultsReadmKfold); dotplot(resultsReadmKfold)
+
+# ----------------------------------------------------------- #
+# LOOCV CV
+# ----------------------------------------------------------- #
+resultsReadmLoocv <- resamples(list(knn = fitKnnLoocvReadm, 
+                                    svm = fitSvmLoocvReadm,
+                                    rf = fitRfLoocvReadm))
+
+summary(resultsReadmLoocv); dotplot(resultsReadmLoocv)
 
 # ----------------------------------------------------------- #
